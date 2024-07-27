85b3847a0ef804ab45ca79a4af2a9e338dbc5993
Leonid <bolleoa@gmail.com>
2024-07-27 10:21:31 +0800

    mypy and def my_write (large files)

--- component_functions.py
+++ component_functions.py
@@ -3,23 +3,23 @@ from constant import const as C
 
 
 def is_eq_name(tuple1: tuple, tuple2: tuple) -> bool:
-    """ Функция is_eq_name(tuple1: tuple, tuple2: tuple) -> bool
-            сравнивает равны ли постоянные части имён компонентов
-            (имя компонента и расширение). Номера версий не сравниваются.
+    """Функция is_eq_name(tuple1: tuple, tuple2: tuple) -> bool
+        сравнивает равны ли постоянные части имён компонентов
+        (имя компонента и расширение). Номера версий не сравниваются.
 
-        Аргументы:
-             tuple1 (tuple): кортеж имени компонента (имя, версия, расширение файла).
-             tuple2 (tuple): кортеж имени компонента (имя, версия, расширение файла).
+    Аргументы:
+         tuple1 (tuple): кортеж имени компонента (имя, версия, расширение файла),
+         tuple2 (tuple): кортеж имени компонента (имя, версия, расширение файла).
 
-        Возвращает:
-             bool: True если постоянные части имён совпадают, иначе - False.
+    Возвращает:
+         bool: True если постоянные части имён совпадают, иначе - False.
     """
 
     return tuple1[0] == tuple2[0] and tuple1[2] == tuple2[2]
 
 
-def name_to_tuple(name: str) -> (str, str, str):
-    """ Функция name_to_tuple(name: str) -> (str, str, str)
+def name_to_tuple(name: str) -> tuple[str, str, str] | None:
+    """Функция name_to_tuple(name: str) -> tuple[str, str, str]
         разбивает имя файла компонента на 3 части:
         (имя компонента, номер версии, расширение файла)
     :param name: str - имя файла компонента
@@ -28,7 +28,15 @@ def name_to_tuple(name: str) -> (str, str, str):
     """
 
     res_match = match(C.RE_PATTERN_NAME_COMPONENT, name)
-    return res_match.groups() if res_match else None
+    if not res_match:
+        return None
+    if not res_match.group(1):
+        return None
+    if not res_match.group(2):
+        return None
+    if not res_match.group(3):
+        return None
+    return res_match.group(1), res_match.group(2), res_match.group(3)
 
 
 def reset_component_version(name_file: str) -> str:
--- path_men.py
+++ path_men.py
@@ -12,116 +12,140 @@ class MyException(Exception):
 
 
 def main_() -> None:
-    """ Функция main() -> None
-            Обрабатывает прерывания сгенерированные в функции main_
+    """Функция main() -> None
+    Обрабатывает прерывания сгенерированные в функции main_
     """
 
-    file_log = logging.FileHandler('Log_path.log')
+    file_log = logging.FileHandler("Log_path.log")
     console_log = logging.StreamHandler()
 
-    logging.basicConfig(level=logging.INFO, handlers=(file_log, console_log),
-                        format="%(asctime)s %(levelname)s %(message)s")
+    logging.basicConfig(
+        level=logging.INFO,
+        handlers=(file_log, console_log),
+        format="%(asctime)s %(levelname)s %(message)s",
+    )
 
     try:
         main()
     except MyException as e:
         logging.error(e.text_err)
         exit(e.return_code)
+
+
 #    except Exception as e:
 #        logging.critical(f'Непредвиденная ошибка\n{e}')
 
 
 def main() -> None:
-    """ Функция main_() -> None
-            Для всех компонентов, имеющих одинаковые имена компонента и расширения файла,
-            перемещает в директорию OLD компоненты с более ранней версией.
-            В результате в директории компонент остаются только самые "свежие" компоненты.
-            Версия компонента определяется из кортежа, формируемого из имени файла компонента
-            (имя, версия, расширение файла)
+    """Функция main_() -> None
+    Для всех компонентов, имеющих одинаковые имена компонента и расширения файла,
+    перемещает в директорию OLD компоненты с более ранней версией.
+    В результате в директории компонент остаются только самые "свежие" компоненты.
+    Версия компонента определяется из кортежа, формируемого из имени файла компонента
+    (имя, версия, расширение файла)
     """
 
     dir_components, sub_dir_oldest, components = get_components()
     components = sorted(components)
     count_outdated = 0
-    name_tuple_previous = ('', '', '')
+    name_tuple_previous = ("", "", "")
 
     for component in components:
         name_tuple = f.name_to_tuple(component.parts[-1])
         if name_tuple:
             if f.is_eq_name(name_tuple, name_tuple_previous):
-                remove_oldest_file(dir_components, sub_dir_oldest,
-                                   get_oldest_component(name_tuple, name_tuple_previous))
+                remove_oldest_file(
+                    dir_components,
+                    sub_dir_oldest,
+                    get_oldest_component(name_tuple, name_tuple_previous),
+                )
                 count_outdated += 1
         else:
-            name_tuple = ('', '', '')
+            name_tuple = ("", "", "")
         name_tuple_previous = name_tuple
 
-    logging.info(f'Обнаружено и перенесено в поддиректорию {C.SUB_DIR_OLD}: '
-                 f'устаревших компонент - {count_outdated}')
+    logging.info(
+        f"Обнаружено и перенесено в поддиректорию {C.SUB_DIR_OLD}: "
+        f"устаревших компонент - {count_outdated}"
+    )
     return None
 
 
-def get_components() -> (Path, Path, list):
-    """ Функция get_components() -> (Path, Path, list):
+def get_components() -> tuple[Path, Path, list]:
+    """Функция get_components() -> (Path, Path, list):
 
-        Возвращает
-            Выбранную из параметров программы директорию компонент.
-            Поддиректорию для устаревших файлов.
-            Список файлов (компонент) выбранной директории.
+    Возвращает
+        Выбранную из параметров программы директорию компонент.
+        Поддиректорию для устаревших файлов.
+        Список файлов (компонент) выбранной директории.
     """
 
     if not 2 <= len(argv) <= 3:
-        raise MyException("Программе передано неверное количество параметров.\n"
-                          "Программа принимает 1 параметр:\n"
-                          "   Имя директории с дистрибутивом обновлений, например,\n"
-                          "   c:\\Дистрибутив\\PREPARE').\n"
-                          "   Допускается второй, необрабатываемый, параметр", 1000)
+        raise MyException(
+            "Программе передано неверное количество параметров.\n"
+            "Программа принимает 1 параметр:\n"
+            "   Имя директории с дистрибутивом обновлений, например,\n"
+            "   c:\\Дистрибутив\\PREPARE').\n"
+            "   Допускается второй, необрабатываемый, параметр",
+            1000,
+        )
 
     dir_components = Path(argv[1])
     try:
-        list_components = dir_components.glob('*.*')
+        list_components = dir_components.glob("*.*")
     except Exception as e:
-        raise MyException(f'Нет доступа к каталогу {dir_components}\n'
-                          f'{e}\n'
-                          f'Обратитесь к системному администратору.', 1000)
+        raise MyException(
+            f"Нет доступа к каталогу {dir_components}\n"
+            f"{e}\n"
+            f"Обратитесь к системному администратору.",
+            1000,
+        )
 
     sub_dir_oldest = Path(dir_components, C.SUB_DIR_OLD)
     try:
         sub_dir_oldest.mkdir(exist_ok=True)
     except Exception as e:
-        raise MyException(f'Нет доступа к каталогу {sub_dir_oldest}\n'
-                          f'{e}\n'
-                          f'Обратитесь к системному администратору.', 1000)
+        raise MyException(
+            f"Нет доступа к каталогу {sub_dir_oldest}\n"
+            f"{e}\n"
+            f"Обратитесь к системному администратору.",
+            1000,
+        )
 
-    for file in sub_dir_oldest.glob('*.*'):
-        raise MyException(f'Директория {sub_dir_oldest} не пуста\n'
-                          'Обработайте и очистите директорию, выполните программу path_men', 1000)
+    for file in sub_dir_oldest.glob("*.*"):
+        raise MyException(
+            f"Директория {sub_dir_oldest} не пуста\n"
+            "Обработайте и очистите директорию, выполните программу path_men",
+            1000,
+        )
 
-    return dir_components, sub_dir_oldest, list_components
+    return dir_components, sub_dir_oldest, list(list_components)
 
 
 def get_oldest_component(tuple1, tuple2) -> tuple:
-    """ Функция get_oldest_component(tuple1, tuple2) -> tuple
-            Cравнивает номера версий компонентов.
+    """Функция get_oldest_component(tuple1, tuple2) -> tuple
+        Cравнивает номера версий компонентов.
 
-        Аргументы:
-            tuple1 (tuple): кортеж имени компонента (имя, версия, расширение файла).
-            tuple2 (tuple): кортеж имени компонента (имя, версия, расширение файла).
+    Аргументы:
+        tuple1 (tuple): кортеж имени компонента (имя, версия, расширение файла).
+        tuple2 (tuple): кортеж имени компонента (имя, версия, расширение файла).
 
-        Результат
-            Картеж имени компонента с более ранней версией.
+    Результат
+        Картеж имени компонента с более ранней версией.
     """
 
     return tuple1 if int(tuple1[1]) < int(tuple2[1]) else tuple2
 
 
-def remove_oldest_file(dir_components: Path, sub_dir_oldest: Path, tuple_: tuple) -> None:
-    """ Функция remove_oldest_file(dir_components: Path, tuple_: tuple) -> None
-            преремещает файл компонента в поддиректорию SUB_DIR_OLD.
+def remove_oldest_file(
+    dir_components: Path, sub_dir_oldest: Path, tuple_: tuple
+) -> None:
+    """Функция remove_oldest_file(dir_components: Path, tuple_: tuple) -> None
+        преремещает файл компонента в поддиректорию SUB_DIR_OLD.
 
-        Аргументы
-            1. dir_components: Path.    Полный путь к директории с компонентами.
-            2. tuple_:         tuple.   Картеж имени перемещаемого файла компонента (имя, версия, расширение файла).
+    Аргументы
+        1. dir_components: Path.    Полный путь к директории с компонентами.
+        2. tuple_:         tuple.   Картеж имени перемещаемого файла компонента (имя, версия, расширение файла).
     """
 
     file_name = tuple_[0] + tuple_[1] + tuple_[2]
@@ -130,10 +154,13 @@ def remove_oldest_file(dir_components: Path, sub_dir_oldest: Path, tuple_: tuple
     try:
         file_from.rename(file_to)
     except Exception as e:
-        raise MyException(f'Не могу копировать файл {file_to}\n'
-                          f'{e}\n'
-                          f'Обратитесь к системному администратору.', 1000)
+        raise MyException(
+            f"Не могу копировать файл {file_to}\n"
+            f"{e}\n"
+            f"Обратитесь к системному администратору.",
+            1000,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main_()
